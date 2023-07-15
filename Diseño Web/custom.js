d8bf92db802a4bcfeba14f340bff90e9fed4153f
Lucas <lucasvilela@hotmail.coma.ar>
2023-07-15 17:49:39 -0300

    ruthjeje

---
+++ Diseño Web/custom.js
@@ -0,0 +1,14 @@
+
+console.log("hola mundo");
+
+document.write ("hola mundo");
+
+console.log ("el resultado es:", 4 + 4)
+
+var numero1=12
+var numero2=3
+console.log (numero1+numero2);
+
+var eldiv=class "uncomme"
+
+
